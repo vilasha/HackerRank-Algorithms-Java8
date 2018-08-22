fccb910bb5f680bac3a9206e45699962324dda1e
vilasha <mariavya@gmail.com>
2018-08-22 01:20:02 +0100

    task 056	Beautiful Triplets (Easy, Max Score: 20, Success Rate: 90.65%)

---
+++ task056.java
@@ -0,0 +1,39 @@
+/*	Beautiful Triplets
+
+	Erica wrote an increasing sequence of  numbers () in her notebook. She considers a triplet  to be beautiful if:
+
+	Given the sequence and the value of , can you help Erica count the number of beautiful triplets in the sequence?
+
+	Input Format
+
+	The first line contains  space-separated integers  and , the length of the sequence and the beautiful difference. 
+	The second line contains  space-separated integers .
+
+	Output Format
+
+	Print a single line denoting the number of beautiful triplets in the sequence.	*/
+	
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
+public class Solution {
+    
+    static int counter;
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            sc.next();
+            int d = sc.nextInt(); sc.nextLine();
+            Set<Integer> sequence = Stream.of(sc.nextLine().split(" "))
+                .map(Integer::parseInt).collect(Collectors.toSet());
+            counter = 0;
+            sequence.forEach(i -> {
+                if (sequence.contains(i-d) && sequence.contains(i+d))
+                    counter++;
+            });
+            System.out.println(counter);
+        }
+    }
+}
